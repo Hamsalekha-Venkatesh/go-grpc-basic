801bfa135e17637377529eee8bb258263d220f1d
Hamsalekha Venkatesh <lekha.hamsa93@gmail.com>
2026-01-08 14:52:46 -0800

    bi-direc streams chat server

--- bi_direc_stream.go
+++ bi_direc_stream.go
@@ -1,41 +1,73 @@
 package main
 
 import (
-	"fmt"
 	pb "grpc_server/proto/stream_gen"
 	"io"
 	"log"
+	"sync"
 	"time"
 )
 
 type BiDirectedServer struct {
 	pb.UnimplementedStreamingServer
-	pb.UnimplementedCalculatorServer
 }
 
-func (bidi *BiDirectedServer) ChatNow(stream pb.Streaming_ChatNowServer) error {
-	for {
-		req, err := stream.Recv()
-		if err == io.EOF {
-			return nil
-		}
+func (bidi *BiDirectedServer) ChatNow(chatStream pb.Streaming_ChatNowServer) error {
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := sendMessages(chatStream)
 		if err != nil {
-			return err
+			log.Fatal("Error sending messages", err)
+			return
 		}
+	}()
 
-		log.Printf("Received: %v", req.GetMessage())
-		// ⚠️ Blocking input (OK for demo only)
-		var reply string
-		fmt.Print("Enter reply: ")
-		_, _ = fmt.Scanln(&reply)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err := receiveMessages(chatStream)
+		if err != nil {
+			log.Fatal("Error receiving messages", err)
+			return
+		}
+	}()
 
-		err = stream.SendMsg(pb.ChatMessage{
-			PersonId:  100,
-			Message:   reply,
-			Timestamp: time.Now().Format(time.RFC3339),
+	wg.Wait()
+	return nil
+}
+
+func sendMessages(stream pb.Streaming_ChatNowServer) error {
+	messages := []string{"Hello", "How're you", " I'm hungry"}
+	for _, message := range messages {
+		err := stream.SendMsg(&pb.ChatMessage{
+			Message:   message,
+			Timestamp: time.DateTime,
+			PersonId:  99,
 		})
+
 		if err != nil {
 			return err
 		}
 	}
+
+	return nil
+}
+
+func receiveMessages(stream pb.Streaming_ChatNowServer) error {
+	for {
+		req, err := stream.Recv()
+		log.Println("\n Received", req.GetMessage())
+		if err == io.EOF {
+			log.Println("client closed stream")
+			break
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
